5cfc2796852b4dd26e861fae79b84f501cb2122f
jonathanrasquin <jonathan.rasquin@gmail.com>
2025-05-08 21:25:11 +0200

    initial commit

---
+++ StockStore/Models/Sock.cs
@@ -0,0 +1,11 @@
+﻿namespace StockStore.Models
+{
+    public class Sock
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ StockStore/Services/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using StockStore.Models;
+
+namespace StockStore.Services
+{
+    public interface IProductService
+    {
+        Task<List<Sock>> GetProductsAsync();
+        Task<Sock> GetProductByIdAsync(int id);
+    }
+}
